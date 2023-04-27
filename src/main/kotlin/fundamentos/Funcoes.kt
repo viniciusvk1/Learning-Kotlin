5d6b6697b3b4837b66f828adf61783b8a655004b
Vinicius Almeida <viniciusalmeida.vk1@gmail.com>
2023-04-27 17:22:08 -0300

    learning about functions

--- src/main/kotlin/fundamentos/Funcoes.kt
+++ src/main/kotlin/fundamentos/Funcoes.kt
@@ -1,5 +1,16 @@
 package fundamentos
 
-fun main(): Unit{
+fun main(): Unit {
     println("Hello World!");
+
+    dizOi(retornaNome())
+
+}
+
+fun retornaNome(): String {
+    return "Vinicius Almeida";
+}
+
+fun dizOi(nome: String, idade: Int = 21) {
+    println("Oi, ${nome}! Parabens pelos seus ${idade} anos!");
 }
