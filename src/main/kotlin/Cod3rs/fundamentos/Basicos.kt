75a4f1371dbacce347b447ebdb1b87662c961850
Vinicius Almeida <viniciusalmeida.vk1@gmail.com>
2023-04-29 17:30:57 -0300

    basics

--- src/main/kotlin/Cod3rs/fundamentos/Basicos.kt
+++ src/main/kotlin/Cod3rs/fundamentos/Basicos.kt
@@ -8,4 +8,18 @@ fun main() {
     val num3: Int = 2147483647
     val num4: Long = 4901249072190479210
 
+    val num5: Float = 3.14F
+    val num6: Double = 3.14
+
+    val char: Char = '?'
+
+    val boolean: Boolean = true // or false
+
+    println(listOf(num1, num2, num3, num4, num5, num6, char, boolean))
+    println(2 is Int)
+    println(2147486381 is Long)
+    println(1.0 is Double)
+
+    println(10.dec())
+    
 }
