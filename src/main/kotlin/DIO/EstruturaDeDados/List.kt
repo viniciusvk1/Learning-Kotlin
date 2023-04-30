c84c74ad3af7062cdade706f0fdebc846c21c0aa
Vinicius Almeida <viniciusalmeida.vk1@gmail.com>
2023-04-30 12:22:21 -0300

    Revert "Revert "list""
    This reverts commit 814efc682ebd600b9129a32864e489b5d6c2fbad.

---
+++ src/main/kotlin/DIO/EstruturaDeDados/List.kt
@@ -0,0 +1,23 @@
+package DIO.EstruturaDeDados
+
+val systemUsers: MutableList<Int> = mutableListOf(1, 2, 3)
+val sudoers: List<Int> = systemUsers
+
+fun addSystemUsers(newUser: Int) {
+    systemUsers.add(newUser)
+}
+
+fun getSysSudoers(): List<Int> {
+    return sudoers
+}
+
+fun main() {
+    addSystemUsers(4)
+    println("Tot sudoers: ${getSysSudoers().size}")
+    getSysSudoers().forEach{
+        i -> println("Soma useful into on user $i")
+    }
+
+ //   getSysSudoers().add(5) <- Error!
+
+}
