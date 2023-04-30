e978af3fed32a81f253cb61aeea9a975b4351392
Vinicius Almeida <viniciusalmeida.vk1@gmail.com>
2023-04-30 15:10:18 -0300

    template string

---
+++ src/main/kotlin/Cod3rs/fundamentos/TemplateString.kt
@@ -0,0 +1,11 @@
+package Cod3rs.fundamentos
+
+fun main() {
+    val aprovados = listOf("Joao", "Maria", "Pedro") // criando uma lista de nomes que permite ser modificada
+    // recebendo o tipo String e 3 nomes aleatorios para o exemplo
+    println("O primeiro colocado foi ${aprovados[0]}!") // mostrando ilutrativamente como se a primeira posicao da lista fosse
+    // o primeiro colocado.
+
+    // testando o conceito de lists
+
+}
---
+++ src/main/kotlin/Cod3rs/fundamentos/TemplateString2.kt
@@ -0,0 +1,7 @@
+package Cod3rs.fundamentos
+
+fun main() {
+    val bomHumor = false; // passa um valor para a variavel bomHumor
+    println("Hoje estou ${if (bomHumor) "feliz" else "chateado"}.") // sendo true a variavel bom humor
+                                        // o programa ira rodar "feliz" e sendo false o programa ira rodar "chateado"
+}
