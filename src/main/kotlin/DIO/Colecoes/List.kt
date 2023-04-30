d2af27e699b3bea84ff9459249114fabedd3ba51
Vinicius Almeida <viniciusalmeida.vk1@gmail.com>
2023-04-30 12:34:07 -0300

    list study

--- src/main/kotlin/DIO/Colecoes/List.kt
+++ src/main/kotlin/DIO/Colecoes/List.kt
@@ -1,23 +1,25 @@
 package DIO.Colecoes
 
-val systemUsers: MutableList<Int> = mutableListOf(1, 2, 3)
-val sudoers: List<Int> = systemUsers
+// Criando uma variavel imutavel systemUsers -> recebendo uma lista mutavel
+val systemUsers: MutableList<Int> = mutableListOf(1, 2, 3) // A variavel so pode receber a instancia da lista inicial
+val sudoers: List<Int> = systemUsers // Criando uma variavel imutavel recebendo a variavel systemUsers
+// em resumo criando uma copia de somente leitura da lista mutableListOf
 
 fun addSystemUsers(newUser: Int) {
-    systemUsers.add(newUser)
+    systemUsers.add(newUser)    // Adiciona um novo usuario na lista "mutableListOf" -> aceita ser manipulado
 }
 
 fun getSysSudoers(): List<Int> {
-    return sudoers
+    return sudoers // retorna a copia de leitura da lista anterior
 }
 
 fun main() {
-    addSystemUsers(4)
-    println("Tot sudoers: ${getSysSudoers().size}")
-    getSysSudoers().forEach{
+    addSystemUsers(4) // adiciona um novo dado a lista que pode ser mutavel
+    println("Tot sudoers: ${getSysSudoers().size}") // mostra a copia da lista mostrando o tamanho de dados que estao presentes na lista original
+    getSysSudoers().forEach{ // percorre todos os elementos da lista
             i -> println("Soma useful into on user $i")
     }
 
-    //   getSysSudoers().add(5) <- Error!
+    //   getSysSudoers().add(5) <- Error! (Impossivel adicionar dados em uma lista imutavel (somente leitura)
 
 }
