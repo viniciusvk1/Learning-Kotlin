27d8c4f6ceb208c9f5216ced40fbe15ed65ac481
Vinicius Almeida <viniciusalmeida.vk1@gmail.com>
2023-05-01 15:37:24 -0300

    exercicies

---
+++ src/main/kotlin/DIO/Exercicios/E5.kt
@@ -0,0 +1,24 @@
+package DIO.Exercicios
+
+data class Pais(var habitantes: Double, val taxaCrescimento: Double) {
+    fun crescerPopulacaoAnual() {
+        val aumento = habitantes * (taxaCrescimento / 100)
+        habitantes += aumento
+    }
+}
+
+fun main() {
+    val habitantesPaisA = readLine()!!.toDouble();
+    val habitantesPaisB = readLine()!!.toDouble();
+    val paisA = Pais(habitantesPaisA, taxaCrescimento = 3.0)
+    val paisB = Pais(habitantesPaisB, taxaCrescimento = 1.5)
+
+    var quantidadeAnos = 0
+    while (paisA.habitantes < paisB.habitantes) {
+        paisA.crescerPopulacaoAnual()
+        paisB.crescerPopulacaoAnual()
+        quantidadeAnos++
+    }
+
+    println("$quantidadeAnos anos")
+}
